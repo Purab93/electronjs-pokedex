19e3c5aca0c168a672270992f12689ecc3527f88
Purab93 <purab@Purabs-MacBook-Pro.local>
2020-06-27 18:27:09 +0530

    base code commit

---
+++ precache-manifest.2954de8c66fd95f6fc293e13f893dd55.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7cdce492b3e5f55ed2fce321ff41dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4d29d7ced57d90b3fde",
+    "url": "/static/css/2.d84e6e44.chunk.css"
+  },
+  {
+    "revision": "ef6f8303a9aa488ac8db",
+    "url": "/static/css/main.8cd3fdcb.chunk.css"
+  },
+  {
+    "revision": "e4d29d7ced57d90b3fde",
+    "url": "/static/js/2.37c8a01f.chunk.js"
+  },
+  {
+    "revision": "ef6f8303a9aa488ac8db",
+    "url": "/static/js/main.b7db294d.chunk.js"
+  },
+  {
+    "revision": "c0f031cf7cc918c2aa1b",
+    "url": "/static/js/runtime-main.0c247358.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
